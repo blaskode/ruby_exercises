7addc6a9c24fef8672544aa4dafc25b513155e6e
Damien Edward Blasko <blaskode@gmail.com>
2020-02-23 09:42:42 -0500

    ex1 now contains 25 problem solutions

--- ex1.rb
+++ ex1.rb
@@ -1,105 +1,24 @@
 
-# (#1-5)
+# (#)
 # Write a function "print_stairs(n, str)" which takes string input and an 
 # integer n and outputs a series of n strings in a geometric pattern resembling 
 # a staircase. 
 # example: 
-
 # print_stairs(5, "aa")
 # => aa
 # => aaaa
 # => aaaaaa
 # => aaaaaaaa
 # => aaaaaaaaaa
-
-# n must be a non-negative integer. 
-# Rewrite the function as many times as necessary until you arrive at good style
-
-#(1) First attempt uses nested recursion. Note that str must be 
-# passed to the inner function because of 'scope gating'
 def print_stairs n, str
-	def helper n, str1, str
-		if n == 0
-		return
-		else
-			puts str1
-			n -= 1
-			str1 += str
-			helper n, str1, str
-		end		
-	end
-	helper(n, str, str)
-end
-
-
-#(2) Replace if...else with a simpler control structure
-# we can use "unless"
-def print_stairs1 n, str
-	def helper n, str1, str
-		unless n == 0
-			puts str1
-			n -= 1
-			str1 += str
-			helper n, str1, str
-		end		
-	end
-	helper(n, str, str)
-end
-
-# (3) eliminate helper function by loading 
-# each line of output into an array then
-# printing the array
-def print_stairs2 n, str
-	s = str;
-	array = []
-	n.times do|n| 
-		array.push(s)
-		s += str
-	end
-	array.each {|e| puts e}
-end
-
-# (4) eliminate the array 
-def print_stairs3 n, str
-	s = str;
-	n.times do|n| 
-		puts s
-		s += str
-	end
-end
-
-# (5) use curly-brace block syntax
-# instead of a multi-line do...end
-def print_stairs4 n, str
 	s = str;
 	n.times {|n| puts s; s += str}
 end
 
-# Finally, we have arrived at good idiomatic Ruby style!
-
-# print_stairs4(5, "aa");
-
-# (6 - 7) Wurst is the best. Create a function that takes a string and replaces 
+# (2) Wurst is the best. Create a function that takes a string and replaces 
 # every mention of any type of sausage with the German word "Wurst," 
 # unless—of course—the sausage is already a type of German "Wurst" 
 # (i.e. "Bratwurst", see below), then leave the sausage name unchanged.
-
-# (6) Convert the sausages, don't worry about punctuation.
-def convert_to_wurst str
-	#wursts = ['bratwurst', 'kochwurst', 'leberwurst', 
-		#'mettwurst', 'rostbratwurst'];
-	not_wursts = ['kielbasa', 'chorizo', 'moronga', 
-		'salami', 'sausage', 'andouille', 'naem',
-		'merguez', 'gurka', 'snorkers', 'pepperoni'];
-	str = str.split;
-	str.map! { |i| not_wursts.include?(i) ? 'Wurst' : i}
-	str.join(" ")
-end
-
-# (7) Now, rewrite it so that it takes punctuation and capitalization
-# into account. A sausage starting with a capital letter, or at the end
-# of a sentence (followed immediately by a period), will trip up (6)
-
 def wurst_is_better str
 	not_wursts = ['kielbasa', 'chorizo', 'moronga', 
 	'salami', 'sausage', 'andouille', 'naem',
@@ -111,13 +30,13 @@ def wurst_is_better str
 	str 
 end
 
-# Create a function that returns true when num1 
+# (3) Create a function that returns true when num1 
 # is equal to num2; otherwise return false
 def is_same_num(num1, num2)
 	num1 == num2
 end
 
-# Create a function that returns the product 
+# (4) Create a function that returns the product 
 # of all odd integers in an array.
 def odd_product(arr)
 	arr.filter! { |i| (i % 2 != 0) }
@@ -125,18 +44,9 @@ def odd_product(arr)
 	return x
 end
 
-# Create a function that takes a string 
+# (5) Create a function that takes a string 
 # and returns a string in which each character is repeated once.
-
 def double_char(str)
-	arr = str.split('')
-	new_arr = []
-	arr.each{|i| new_arr.push(i); new_arr.push(i)}
-	str = new_arr.join()
-	return str;
-end
-
-def double_char1(str)
 	arr = str.split('')
 	new_arr = []
 	arr.each do |i| 
@@ -146,50 +56,32 @@ def double_char1(str)
 	return str;
 end
 
-# Write a function that returns true 
+# (6) Write a function that returns true 
 # if a hash contains the specified key, and false
 # otherwise 
 def has_key(hash, key)
 	hash.has_key? key
 end
 
-def factorial(int)
-	if int == 0
-		1
-	else
-		int * factorial(int - 1)
-	end
-end
 
-def factorial1(int)
+# (7) Computer the factorial of an integer
+def factorial(int)
 	(1..int).inject(:*)
 end
 
-# Create a function that takes a string and 
+# (8) Create a function that takes a string and 
 # returns the number (count) of vowels contained within it.
-def count_vowels(str)
-	vowels = ['a', 'e', 'i', 'o', 'u']
-	str = str.split('')
-	count = 0;
-	str.each do |i|
-		if vowels.include? i
-			count += 1
-		end
-	end
-	p count 
-end
-
-def count_vowels1(s)
+def count_vowels(s)
 	p s.count('aeiou')
 end
 
-# Create a function that takes an array 
+# (9) Create a function that takes an array 
 # of names and returns an array with only the first letter capitalized.
 def cap_me(arr)
 	arr.map { |i| i.capitalize}
 end
 
-# Create a function that takes an array as an argument and returns true or false depending 
+# (10) Create a function that takes an array as an argument and returns true or false depending 
 # on whether the average of all elements in the array is a whole number or not.
 def is_avg_whole?(arr)
 	sum = arr.sum
@@ -198,14 +90,165 @@ def is_avg_whole?(arr)
 	avg_i == avg_f
 end
 
+# (11) A syllable is defined as a repetitive sequence containing a vowel
+# ex: lalala contains three syllables
+# count the syllables in a given string
+def count_syllables(str)
+	str = str.downcase
+	arr = str.split('')
+	sentry = arr[0]
+	syllable = arr[0]
+	arr.shift
+	arr.each do |n| 
+		if n == sentry
+			break 
+		else
+		 syllable += n 
+		end
+	end
+	str.scan(syllable).size
+end
+
+# (12) Format a given string of ten numbers such that it resembles
+# a phone number: (xxx) xxx-xxxx
+def format_phone_number(arr)
+	phone_num = "("
+	3.times {phone_num += arr.shift.to_s}
+	phone_num += ") "
+	3.times {phone_num += arr.shift.to_s}
+	phone_num += "-"
+	4.times {phone_num += arr.shift.to_s}
+	phone_num
+end
+
+# (13) Given an array of drink-names, return an array
+# that does not contain 'cola' or 'fanta'
+def skip_the_sugar(drinks)
+	too_much = ['cola', 'fanta']
+	drinks.select! { |n| !too_much.include? n}
+	drinks
+end
+
+# (14) Create a function that outputs true if a number is prime, 
+# and false otherwise.
+def isPrime num
+	if num == 1
+		return false
+	end
+	stop = num - 1
+	(2..stop).each do |n|  
+		if (num % n) == 0 
+			return false
+		end
+	end
+	return true
+end
+
+# (15) Write a function that takes three arguments (x, y, z) and 
+# returns an array containing x subarrays (e.g. [[], [], []]), 
+# each containing y number of item z.
+# x Number of subarrays contained within the main array.
+# y Number of items contained within each subarray.
+# z Item contained within each subarray.
+
+def matrix(x, y, z)
+  sub_array = []
+  y.times do
+    sub_array.push(z);
+  end
+  #sub_array
+  array = [];
+  x.times do
+    array.push(sub_array)
+  end
+  array
+end
+
+# (17) add the consecutive integers up and including
+# the input. eg, add_up(5) = 16
+def add_up(num)
+	sum = 0;
+	(1..num).each { |x| sum += x }
+	sum
+end
+
+# (18) return the smaller of two integers, but do not
+# convert from string to integer unless necessary
+def smaller_num(n1, n2)
+	if n1.length > n2.length
+		n2
+	elsif n2.length > n1.length
+		n1
+	else
+		n1.to_i > n2.to_i ? n2 : n1 
+	end
+end
+
+# (19) return pi to n decimal places
+def my_pi(n)
+	#p Math::PI
+	Math::PI.round(n)
+end
+
+# (20) sum all even integers within a range of two numbers,
+# the sum should be inclusive of both the first and last even
+# number in the rangea
+def sum_even_nums_in_range(start, stop)
+	sum = 0;
+	a = (start..stop).select { |n| n.even? }
+	sum = a.sum
+	sum
+end
+
+# (21) find the maximum of the frist n elements of an array of numbers
+def max_total(nums, n)
+	nums.sort!
+	nums.reverse!
+	sum = 0;
+	n -= 1;
+	(0..n).each{|n| sum += nums[n]}
+	sum 
+end
 
 
+# (22) Determine if two strings are anagrams of each other
+def is_anagram(s1, s2)
+	s1 = s1.downcase.split('').sort
+	s2 = s2.downcase.split('').sort
+	s1 == s2 ? true : false 	 
+end
 
+# (23) Determine if all numbers in an array are even
+# and return true or false
+def checkAllEven(arr)
+  return arr.all?{|num| num.even?}
+end
 
+# (24) Sum the absolute values of the elements
+# in an array
+def get_abs_sum(arr)
+	sum = 0;
+	arr.each { |n| sum += n.abs}
+	sum
+end
 
-
-
-
+# (25) return "true" if "Frodo" and "Sam" are 
+# adjacent elements in a string array
+def middle_earth(arr)
+	#arr = arr.sort
+	start = 0
+	finish = arr.length - 2
+	truthy = false
+	(start..finish).each do |n| 
+		if (arr[n] == "Frodo" and arr[n + 1] == "Sam")
+			truthy = true
+		end
+		if (arr[n] == "Sam" and arr[n + 1] == "Frodo")
+			truthy = true
+		end
+	end
+	truthy
+end
 
 
 
